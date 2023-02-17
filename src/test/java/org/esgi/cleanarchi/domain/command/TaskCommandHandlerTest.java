7ca723a160a8071e54339232349b8a4f92a2f722
Rémy Machavoine <remygt@hotmail.fr>
2023-02-17 23:54:14 +0100

    add usecase inMemory testing start

--- src/test/java/org/esgi/cleanarchi/domain/command/TaskCommandHandlerTest.java
+++ src/test/java/org/esgi/cleanarchi/domain/command/TaskCommandHandlerTest.java
@@ -6,6 +6,7 @@ import org.esgi.cleanarchi.infrastructure.InMemoryTaskRepository;
 import org.esgi.cleanarchi.kernel.Logger;
 import org.junit.Before;
 import org.junit.Test;
+import org.testng.annotations.BeforeMethod;
 
 public class TaskCommandHandlerTest {
 
@@ -26,5 +27,18 @@ public class TaskCommandHandlerTest {
         );
 
         assert idCreated == 4;
+    }
+
+    @Test
+    public void updateTaskTest() {
+        this.taskCommandHandler.updateTask(new UpdateTaskCommand(1, "description", null, null));
+        assert true;
+    }
+
+    @Test
+    public void removeTaskTest() {
+        this.taskCommandHandler.deleteTask(1);
+        assert true;
+
     }
 }
