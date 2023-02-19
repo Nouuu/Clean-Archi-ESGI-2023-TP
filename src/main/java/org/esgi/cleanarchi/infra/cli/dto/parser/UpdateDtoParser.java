fe39040473b33ccace2556bbd6aebcdf8ec8e731
Rémy Machavoine <remygt@hotmail.fr>
2023-02-19 21:36:48 +0100

    delete unnecessary feature

--- src/main/java/org/esgi/cleanarchi/infra/cli/dto/parser/UpdateDtoParser.java
+++ src/main/java/org/esgi/cleanarchi/infra/cli/dto/parser/UpdateDtoParser.java
@@ -14,13 +14,13 @@ import java.util.Optional;
 public class UpdateDtoParser {
     public UpdateDto parse(List<String> args) {
         if (args.size() == 0) {
-            return new UpdateDto(null, null, null, null, null);
+            return new UpdateDto(null, null, null, null);
         } else {
             try {
                 int idToRemove = Integer.parseInt(args.get(0));
-                return new UpdateDto(idToRemove, parseContent(args), parseDueDate(args), parseTaskState(args), parseParentId(args));
+                return new UpdateDto(idToRemove, parseContent(args), parseDueDate(args), parseTaskState(args));
             } catch (IllegalArgumentException exception) {
-                return new UpdateDto(null, parseContent(args), parseDueDate(args), parseTaskState(args), parseParentId(args));
+                return new UpdateDto(null, parseContent(args), parseDueDate(args), parseTaskState(args));
             }
         }
     }
@@ -67,19 +67,6 @@ public class UpdateDtoParser {
                 return null;
             }
         } else {
-            return null;
-        }
-    }
-
-    private Integer parseParentId(List<String> args) {
-        Optional<String> parentId = args.stream().filter(s -> s.startsWith("-p:")).findFirst();
-        if (parentId.isEmpty()) {
-            return null;
-        }
-        try {
-            Integer parentIdCast = Integer.parseInt(parentId.get().split("-p:")[1]);
-            return parentIdCast;
-        } catch (Exception e) {
             return null;
         }
     }
