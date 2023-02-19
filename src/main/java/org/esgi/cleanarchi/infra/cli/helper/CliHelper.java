aaab21a0ae9cb4079f6f0f0ecc1f4f7a4c5f6379
pooetitu <pooetitu@gmail.com>
2023-02-19 16:29:18 +0100

    fix color encoding

--- src/main/java/org/esgi/cleanarchi/infra/cli/helper/CliHelper.java
+++ src/main/java/org/esgi/cleanarchi/infra/cli/helper/CliHelper.java
@@ -9,7 +9,7 @@ public class CliHelper {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_BLUE_DARKER = "\u001B[34m;1";
+    public static final String ANSI_BLUE_DARKER = "\u001B[34;1m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     private final Writer writer;
