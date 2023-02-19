b642ab66cdd062708a8ad9d29ace1e5384969fba
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-02-19 14:49:21 +0100

    console error writer

---
+++ src/main/java/org/esgi/cleanarchi/infra/io/ConsoleErrorWriter.java
@@ -0,0 +1,11 @@
+package org.esgi.cleanarchi.infra.io;
+
+import org.esgi.cleanarchi.infra.io.exception.InputOutputException;
+
+public class ConsoleErrorWriter implements Writer{
+
+    @Override
+    public void write(String s) throws InputOutputException {
+        System.err.println(s);
+    }
+}
