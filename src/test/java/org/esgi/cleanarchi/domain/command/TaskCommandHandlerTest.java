d43a3a2729abac1295593eef35eeb4cb8884c01f
Swann HERRERA <swann@devloup.dev>
2023-02-19 16:01:15 +0100

    try to pass test with fixture

--- src/test/java/org/esgi/cleanarchi/domain/command/TaskCommandHandlerTest.java
+++ src/test/java/org/esgi/cleanarchi/domain/command/TaskCommandHandlerTest.java
@@ -4,10 +4,11 @@ import org.esgi.cleanarchi.domain.Task;
 import org.esgi.cleanarchi.domain.TaskRepository;
 import org.esgi.cleanarchi.domain.TaskState;
 import org.esgi.cleanarchi.infra.FakeLogger;
-import org.esgi.cleanarchi.infra.InMemoryTaskRepository;
 import org.esgi.cleanarchi.infra.MockReader;
 import org.esgi.cleanarchi.infra.MockWriter;
 import org.esgi.cleanarchi.infra.data.JsonTaskRepository;
+import org.esgi.cleanarchi.infra.io.Reader;
+import org.esgi.cleanarchi.infra.io.Writer;
 import org.esgi.cleanarchi.kernel.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -26,24 +28,18 @@ class TaskCommandHandlerTest {
     TaskRepository taskRepository;
     TaskCommandHandler taskCommandHandler;
 
+    Writer writer;
+    MockReader reader;
+
     @BeforeEach
     public void setUp() throws IOException {
         Logger logger = new FakeLogger();
-        this.taskRepository = new JsonTaskRepository(new MockReader(), new MockWriter());
-        this.taskCommandHandler = new TaskCommandHandler(taskRepository, logger);
-        injectFixture();
-    }
-
-    private void injectFixture() {
-
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-        List<Task> tasks = List.of(
-            Task.of(1, "init a project to create an app for my tasks", ZonedDateTime.parse("2022-02-15T22:14:30.486798+01:00", formatter), Optional.ofNullable(ZonedDateTime.parse("2022-02-15T22:14:30.486798+01:00", formatter)), Optional.empty(), TaskState.TODO, null),
-            Task.of(2, "make a design of my needs to manage my tasks", ZonedDateTime.parse("2022-02-13T22:14:30.536023+01:00", formatter), Optional.ofNullable(ZonedDateTime.parse("2022-02-16T21:14:30.536024+01:00", formatter)), Optional.empty(), TaskState.PROGRESS, null),
-            Task.of(3, "brainstorm on what project to work on", ZonedDateTime.parse("2022-01-16T22:14:30.536143+01:00", formatter), Optional.ofNullable(ZonedDateTime.parse("2022-02-16T21:14:30.536024+01:00", formatter)), Optional.ofNullable(ZonedDateTime.parse("2022-02-11T22:14:30.536144+01:00", formatter)), TaskState.CLOSED, null)
-        );
-
-        tasks.forEach(task -> taskRepository.save(task));
+        reader = new MockReader();
+        writer = new MockWriter();
+        String fixture = Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("taskFixture.json")).toString();
+        reader.setNextContent(fixture);
+        taskRepository = new JsonTaskRepository(reader, writer, logger);
+        taskCommandHandler = new TaskCommandHandler(taskRepository, logger);
     }
 
     @Test
